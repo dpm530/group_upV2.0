94e18f5fe47ed7a5c1a12155b3200d061c884b25
Daniel Palacios <danielpalacios530@gmail.com>
2019-08-14 04:37:41 -0500

    added db seeds guest account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.production?
+   User.create!(first_name: 'Guest', last_name: 'Account', email: 'guest@mymedical.com', password: '123456', password_confirmation: '123456')
+end
